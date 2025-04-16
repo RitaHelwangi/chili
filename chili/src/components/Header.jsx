ac8a09ab08689c7a8b56627d8e0ab621a680f3a2
AmirFront-hub <amiralziyadi@gmail.com>
2025-04-16 11:01:09 +0200

    Styling Header

--- chili/src/components/Header.jsx
+++ chili/src/components/Header.jsx
@@ -1,31 +1,29 @@
-import React from 'react';
-import './header.css';
-import Logo from '../assets/Logo.png';
+import React from "react";
+import "./header.css";
+import Logo from "../assets/Logo.png";
 
 const Header = () => {
-    return (
-        <header className="header">
-            <div className="nav-container">
-                <img className="logo" src={Logo} alt="Logo" />
-                <nav>
-                    <ul className="nav-links">
-                        <li>
-                            <a href="#food">Food</a>
-                        </li>
-                        <li>
-                            <a href="#wines">Wines</a>
-                        </li>
-                        <li>
-                            <a href="#bat">Book a Table</a>
-                        </li>
-                        <li>
-                            <a href="#for-workers" className="FWorkers">For Workers</a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
-    );
+  return (
+    <header className="header">
+      <div className="header-container">
+        <div className="header-content">
+          <div className="logo-column">
+            <img className="logo" src={Logo} alt="Logo" />
+          </div>
+          <div className="nav-column">
+            <nav className="nav-links">
+              <div className="nav-item food">Food</div>
+              <div className="nav-item wines">Wines</div>
+              <div className="nav-item book-table">Book table</div>
+            </nav>
+          </div>
+          <div className="workers-column">
+            <div className="for-workers">For workers</div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
