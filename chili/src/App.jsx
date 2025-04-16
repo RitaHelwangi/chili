405640f1cc239fc36be6563cd715e137908688c9
AmirFront-hub <amiralziyadi@gmail.com>
2025-04-16 13:04:40 +0200

    Added new Files,CSS Styling
    Added in React extra function for the Main content and paired the header and main together inside app.jsx

--- chili/src/App.jsx
+++ chili/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import WelcomeSection from "./components/welcomesection";
 
 const App = () => {
     return (
         <div>
             <Header />
+            <WelcomeSection />
         </div>
     );
 };
