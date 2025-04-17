b3a178b13de0244ef236e704d6ae7f0b5d6f9e29
Emma Malinsdotter <emma.malinsdotter@outlook.com>
2025-04-17 10:22:01 +0200

    Uppdaterade app.jsx med det från Form
    la till mer kod i dev från Form

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,26 @@ import React from "react";
 import Menu from "./pages/Menu";
 import { HashRouter,Routes,Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Form from './components/Form'
+
+
+function AppWrapper() {
+	const location = useLocation();
+	const isFormPage = location.pathname === "/form";
+  
+	return (
+	  <>
+		
+		{!isFormPage && (
+		  <NavLink to="/form">Go to Form</NavLink>
+		)}
+		
+		<Routes>
+		  
+		</Routes>
+	  </>
+	);
+  }
 
 function App() {
   return (
