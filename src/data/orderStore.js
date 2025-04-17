aedc6dd8bd9847696a686a5fc6082b3ebeb21479
Rita Helwangi <rita.helwangi@gmail.com>
2025-04-17 09:05:18 +0200

    Implement order management store using Zustand

---
+++ src/data/orderStore.js
@@ -0,0 +1,34 @@
+import { create } from "zustand";
+
+export const useOrderStore = create((set) => ({
+  cart: [],
+
+  addToCart: (pizza) =>
+    set((state) => {
+      const exists = state.cart.find((item) => item.id === pizza.id);
+      if (exists) {
+        return {
+          cart: state.cart.map((item) =>
+            item.id === pizza.id
+              ? { ...item, quantity: item.quantity + 1 }
+              : item
+          ),
+        };
+      }
+      return { cart: [...state.cart, { ...pizza, quantity: 1 }] };
+    }),
+
+  removeFromCart: (pizzaId) =>
+    set((state) => ({
+      cart: state.cart.filter((item) => item.id !== pizzaId),
+    })),
+
+  updateQuantity: (pizzaId, quantity) =>
+    set((state) => ({
+      cart: state.cart.map((item) =>
+        item.id === pizzaId ? { ...item, quantity } : item
+      ),
+    })),
+
+  clearCart: () => set({ cart: [] }),
+}));
