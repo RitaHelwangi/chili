0f217ded7a03dcd8699ed3b408093384078f5343
Akevonwolfe <94.andreas.svensson@gmail.com>
2025-04-23 23:42:22 +0200

    added to empty page

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,8 @@ function App() {
   return (
     <>
 
+	<HashRouter>
+		
 	<Header />
 
 		<Routes>
--- src/components/TomSida.jsx
+++ src/components/TomSida.jsx
@@ -1,8 +1,11 @@
+import AddPizzaForm from "./Addpizzaitem";
+
 function TomSida() {
 	return (
-	  <div>
-		{/* Det här är den tomma sidan man kommer till när man klickar på log in */}
-	  </div>
+	  <>
+	  <AddPizzaForm></AddPizzaForm>
+		
+	  </>
 	);
   }
   
