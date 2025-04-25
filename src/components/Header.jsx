453cb95296aa36e77dc8dd67737f112ed8479dda
KoiOriginal <photsathon.sangsukwaw@gmail.com>
2025-04-25 15:41:20 +0200

    Apply changes after merge and stash pop

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-// src/components/Header.jsx
-
 import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../assets/logo.png'
@@ -20,47 +17,3 @@ export default function Header() {
     </header>
   )
 }
-=======
-import React from "react";
-import "./header.css";
-import Logo from "../assets/Logo.png";
-import { NavLink } from "react-router-dom";
-
-const Header = () => {
-  return (
-    <header className="header">
-      <div className="header-container">
-        <div className="header-content">
-
-			
-          <NavLink to="/">
-            <img className="logo" src={Logo} alt="Logo" />
-          </NavLink>
-
-
-          <div className="nav-texts">
-            <NavLink
-              to="/menu"
-              className={({ isActive }) =>
-                isActive ? "nav-item active" : "nav-item"
-              }>Menu</NavLink>
-            
-              
-            
-            <NavLink
-              to="/form"
-              className={({ isActive }) =>
-                isActive ? "nav-item active" : "nav-item"
-              } >Employees</NavLink>
-           
-            
-              
-          </div>
-        </div>
-      </div>
-    </header>
-  );
-};
-
-export default Header;
->>>>>>> dev
