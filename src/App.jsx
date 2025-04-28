2898c9db3ef67fdae9911b70a79ea2de2a4c3ce5
paria <pariyataba@gmail.com>
2025-04-28 15:29:53 +0200

    publish

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Menu from "./pages/Menu";
 import { HashRouter,Routes,Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header.jsx"
-import WelcomeSection from "./components/WelcomeSection.jsx"
 import Footer from "./components/Footer.jsx";
 import Order from './pages/Order';
 import Form from './components/Form'
