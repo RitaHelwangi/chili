fb302a0d8864de334dd4219873d403a3318a7e0a
Rita Helwangi <rita.helwangi@gmail.com>
2025-04-28 15:28:32 +0200

    deploy from dev

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import ghPages from "vite-plugin-gh-pages";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/chili/'
-})
+  plugins: [react(), ghPages()],
+  base: "/my-chili/",
+});
